279b06db09834ffee2665e6dc34bcbe1cc08ff6f
　pingc <1098971850@qq.com>
2021-08-04 21:25:24 +0800

    资产部分完成

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -1,13 +1,20 @@
 package com;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@ServletComponentScan
 @SpringBootApplication
+@EnableScheduling
+@ServletComponentScan
+@MapperScan("com.minimalist.*.mapper")
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class,args);
+        SpringApplication.run(Application.class ,args);
     }
 }
